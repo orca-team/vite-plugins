6c6f17c9458a84be6058a85fbebe785b6cf54081
wang.kangning <wb-wkn507465@alibaba-inc.com>
2023-09-04 13:35:15 +0800

    chore: 发送消息配置

---
+++ emit-log.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# 取出 log.txt 的文件内容
+log=$(cat log.txt|grep 'New tag:')
+
+# 如果 log.txt 为空
+if [ -z "$log" ]; then
+  curl 'https://oapi.dingtalk.com/robot/send?access_token=${DING_TOKEN}' \
+   -H 'Content-Type: application/json' \
+   -d '{"msgtype": "markdown","markdown": {"title":"orca-fe发布通知","text":"### @orca-fe/vite-plugins\n流水线结束，没有新的模块发布"}}'
+  exit 0
+fi
+
+# 如果 log.txt 不为空，则将 log 作为消息发出去，需要将 log 的换行符替换为 \n
+log=${log//$'\n'/\\n}
+curl 'https://oapi.dingtalk.com/robot/send?access_token=${DING_TOKEN}' \
+   -H 'Content-Type: application/json' \
+   -d '{"msgtype": "markdown","markdown": {"title":"orca-fe发布通知","text":"### @orca-fe/vite-plugins\n流水线结束，以下模块已发布：\n'"$log"'"}}'
+
